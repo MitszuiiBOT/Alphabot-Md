82626b64b6887d8e1a38de8bbad99759ccd9d5c1
MitszuiiBOT <97212890+MitszuiiBOT@users.noreply.github.com>
2022-01-10 19:55:55 +0700

    Update confat.js

--- confat.js
+++ confat.js
@@ -3,10 +3,10 @@ const chalk = require('chalk')
 const setting = JSON.parse(fs.readFileSync('./setting.json'));
 
 // Other
-global.owner = ["62887435047326","62887435047326"]
-global.packname = 'ZeeoneOfc'
+global.owner = ["6287869993055","6289654152584"]
+global.packname = 'Mitszuii BOT'
 global.youtube = 'https://youtube.com/c/ZeeoneOfc'
-global.author = '© Alphabot MD'
+global.author = '© MITSZUII BOT ™'
 global.sessionName = 'session'
 global.flaming = "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text="
 global.prefa = ['','!','.','🐦','🐤','🗿']
